ce9f8dbfc6b5c5868039f5901bbe0b709c7c8fee
James Hagood <cardfaux@tutanota.com>
2020-04-06 11:04:48 -0500

    added axios and semantic ui

--- API/Program.cs
+++ API/Program.cs
@@ -12,36 +12,37 @@ using Persistence;
 
 namespace API
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+      var host = CreateHostBuilder(args).Build();
+
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        try
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<DataContext>();
-                    context.Database.Migrate();
-                }
-                catch (Exception ex)
-                {
-                    
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An Error Occured During Migration");
-                }
-            }
-
-            host.Run();
+          var context = services.GetRequiredService<DataContext>();
+          context.Database.Migrate();
         }
+        catch (Exception ex)
+        {
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "An Error Occured During Migration");
+
+        }
+      }
+
+      host.Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
